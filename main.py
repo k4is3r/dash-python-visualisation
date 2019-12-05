107662f1135e4706e5fd4cbed58ad48f7de08733
Eduardo Imery <ejwimery@gmail.com>
2019-12-04 22:26:03 -0600

    Config and importing libraries, create server to show the dashboard

---
+++ main.py
@@ -0,0 +1,16 @@
+#Eduardo Imery
+#04-12-2019
+#importing libraries for dash
+import dash
+from dash.dependenies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+#some controller
+import managerdb.py
+
+#activating the dash server
+app = dash.Dash(__name__)
+server = app.server
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
